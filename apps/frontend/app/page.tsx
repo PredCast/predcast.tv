a4ad3bfbc9a8c92c4cca347818dab58eb3225c21
Antonybyrt <antony.loussararian@gmail.com>
2026-05-28 00:32:10 +0200

    front redeploy

--- apps/frontend/app/page.tsx
+++ apps/frontend/app/page.tsx
@@ -2,6 +2,7 @@ import { Header } from "@/components/Header";
 import { Footer } from "@/components/Footer";
 import { DiscoverPage } from "@/components/features/discover";
 
+// home
 export default function Home() {
   return (
     <main>
