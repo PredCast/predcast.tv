d65ac6a68feb0f30e78fa7bf31de0a5db0f15585
Antonybyrt <antony.loussararian@gmail.com>
2026-05-29 19:16:04 +0200

    front

--- apps/frontend/app/page.tsx
+++ apps/frontend/app/page.tsx
@@ -1,7 +1,7 @@
 import { Header } from "@/components/Header";
 import { Footer } from "@/components/Footer";
 import { DiscoverPage } from "@/components/features/discover";
-
+ 
 export default function Home() {
   return (
     <main>
